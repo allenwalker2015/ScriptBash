6c66b7f215cafd2979c983ab809ea3edcda26448
allenwalker2015 <00053812@uca.edu.sv>
2015-06-22 13:09:08 -0600

    #2 Se puede buscar por algunos tipos

--- Getopts.sh
+++ Getopts.sh
@@ -3,18 +3,68 @@
 SKIPBLANKS=
 TMPDIR=/tmp
 CASE=lower
-#CONT=0
+
+#$1
+
+           if [ -d $1 ]
+           then
+
+           echo 'Si lo haceeeeeeeeee'
+           fi 
 
 while getopts :bt:u arg
 do
    case $arg in
       b)   SKIPBLANKS=TRUE ;;
+      # c)   if [ -d "$OPTARG" ]
+         #  then
+         #  echo 'Se selecciono la opcion u'
+         #  fi ;;
       t)   if [ -d "$OPTARG" ]
            then
               TMPDIR=$OPTARG
 		echo "El valor de TMPDIR: "$TMPDIR
-		
 		var1=`ls -l ${TMPDIR} | egrep -c ^-`
+
+		find ${TMPDIR} -type f | egrep "txt" > /home/ronaldo/Escritorio/txt.txt
+		txt=`egrep -c "*.txt" /home/ronaldo/Escritorio/txt.txt`
+
+		find ${TMPDIR} -type f | egrep "pdf" > /home/ronaldo/Escritorio/pdf.txt
+		pdf=`egrep -c "pdf" /home/ronaldo/Escritorio/pdf.txt`
+
+		find ${TMPDIR} -type f | egrep "mp3" > /home/ronaldo/Escritorio/mp3.txt
+		mp3=`egrep -c "mp3" /home/ronaldo/Escritorio/mp3.txt`
+
+		find ${TMPDIR} -type f | egrep "png" > /home/ronaldo/Escritorio/png.txt
+		png=`egrep -c "png" /home/ronaldo/Escritorio/png.txt`
+
+		find ${TMPDIR} -type f | egrep "odt" > /home/ronaldo/Escritorio/odt.txt
+		odt=`egrep -c "odt" /home/ronaldo/Escritorio/odt.txt`
+
+		#find ${TMPDIR} -type f | egrep "*" > /home/ronaldo/Escritorio/otros.txt
+		#otros=`egrep -c "*" /home/ronaldo/Escritorio/otros.txt`
+
+      suma=$(($txt+$pdf+$mp3+$png+$odt))
+      otros=$(($var1-$suma))
+
+		echo "La cantidad de ficheros es: "${var1}
+		echo "lo que guarda txt es: "${txt}
+      echo "lo que guarda pdf es: "${pdf}
+      echo "lo que guarda mp3 es: "${mp3}
+      echo "lo que guarda png es: "${png}
+      echo "lo que guarda odt es: "${odt}
+      echo "lo que guarda otros es: "${otros}
+		echo "Lo que guarda varl1 es: "$varl1
+		
+#- archivos regulares
+#d directorios
+#c dispositivos de caracter
+#b dispositivos de bloque
+#s sockets
+#p tuberías
+#l links		
+	
+		#var1=`ls -l ${TMPDIR} | egrep -c ^-`
 		var2= ls -l ${TMPDIR} | egrep -c ^d
                 var3= ls -l ${TMPDIR} | egrep -c ^d
                 var4= ls -l ${TMPDIR} | egrep -c ^d
@@ -22,24 +72,7 @@ do
                 var6= ls -l ${TMPDIR} | egrep -c ^d
                 var7= ls -l ${TMPDIR} | egrep -c ^d
                 var8= ls -l ${TMPDIR} | egrep -c ^d
-	
-
-		#var= find ${TMPDIR} -type f | ls -l ${TMPDIR} #| egrep -c ^-
-
- 		#M=1
-		#find $TMPDIR -type f -exec ls -l {} \; | while read dir ; do
-		#	#let X=$X+1
-		#	((++M))
-		#	echo "Valor de dir" $dir
-		#done
-		#echo "El valor de X es: "$M
-
-		echo "La cantidad de ficheros es: "${var1}
-		#var=find $OPTARG -type f 
-		#echo "Los datos encontrados son"$var		
-
-
-
+		
               echo 'Se selecciono la opcion d' 
            else
               echo "$0: $OPTARG no es un directorio." >&2
@@ -57,9 +90,6 @@ done
 echo 'skip: '$SKIPBLANKS
 echo 'tmp: '$TMPDIR
 echo 'case: '$CASE
-
-#((++CONT))
-echo "El contador es: "$CONT
 
 exit 0
 
