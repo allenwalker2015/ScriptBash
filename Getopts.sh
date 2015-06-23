b4784e57a59034baa93754477cd3b485f6d5f980
allenwalker2015 <00053812@uca.edu.sv>
2015-06-22 21:16:18 -0600

    Nuevos cambios

--- Getopts.sh
+++ Getopts.sh
@@ -1,199 +1,303 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-SKIPBLANKS=
-TMPDIR=/tmp
-CASE=lower
-
-#$1
-
-if [ "$0"  == ./Getopts.sh ]; then
-echo "Es getoptsssssssssss"
+ls /var/Proyecto 1>/dev/null 2>/dev/null
+if [ ! $? == 0 ]; then #no existe esa carpeta
+   mkdir /var/Proyecto 2>/dev/null
+   #echo "*******?"$?
+   if [ ! $? == 0 ]; then
+      echo "Error."
+      echo "NO SE PUEDE CREAR. VERIFIQUE SUS PERMISOS."
+      echo "Sus permisos para el directorio /var/ deben de ser: drwxrwxrwx"
+      echo "Sus permisos para el directorio /etc/ deben de ser: drwxrwxrwx"
+      exit 1
+   fi
 fi
-if [ -d "$1" ] ; then
-echo "Es un directoriooooo" 
-fi
 
-         #  if [ -d $1 ]
-          # then
+#VARIABLES CAMBIABLES
+AYUDA=`grep "^AYUDA" /etc/VariablesConfigurables.sh | sed 's/AYUDA=\(.*\)/\1/g'`
+PORCENTAJE=`grep "^PORCENTAJE" /etc/VariablesConfigurables.sh | sed 's/PORCENTAJE=\(.*\)/\1/g'`
+DPROFUNDIDAD=`grep "^DPROFUNDIDAD" /etc/VariablesConfigurables.sh | sed 's/DPROFUNDIDAD=\(.*\)/\1/g'`
+TMPDIR=
 
-           #echo 'Si lo haceeeeeeeeee'
-           #fi 
+function AYUDA(){
+   echo "*Mostrar sólo ayuda*"
+}
 
-while getopts :bt:u arg
-do
-   case $arg in
-      b)   SKIPBLANKS=TRUE ;;
-      # c)   if [ -d "$OPTARG" ]
-         #  then
-         #  echo 'Se selecciono la opcion u'
-         #  fi ;;
-      t)   if [ -d "$OPTARG" ]
-           then
-              TMPDIR=$OPTARG
-		echo "El valor de TMPDIR: "$TMPDIR
-		var1=`ls -l ${TMPDIR} | egrep -c ^-`
+function PROCESO(){
+   ArchivosReglaresT_A=`find ${TMPDIR} -maxdepth 1 -type f 2>/dev/null`
 
-		find ${TMPDIR} -type f | egrep "txt" > /home/ronaldo/Escritorio/txt.txt
-		txt=`egrep -c "*.txt" /home/ronaldo/Escritorio/txt.txt`
+   if [ ! $? == 0 ]; then
+      date > /var/Proyecto/ArchivoProyecto
+      echo "Error." > /var/Proyecto/ArchivoProyecto
+      echo "Su computadora no cumple con los requisitos mínimos para poder ejecutar este comando." > /var/Proyecto/ArchivoProyecto
+      #echo "Error." 
+      #echo "Su computadora no cumple con los requisitos mínimos para poder ejecutar este comando."
+      logger -p auth.error -t PROYECTO_IASGL No cumple requerimientos mínimos
+      exit 1
+   fi
 
-		find ${TMPDIR} -type f | egrep "pdf" > /home/ronaldo/Escritorio/pdf.txt
-		pdf=`egrep -c "pdf" /home/ronaldo/Escritorio/pdf.txt`
-
-		find ${TMPDIR} -type f | egrep "mp3" > /home/ronaldo/Escritorio/mp3.txt
-		mp3=`egrep -c "mp3" /home/ronaldo/Escritorio/mp3.txt`
-
-		find ${TMPDIR} -type f | egrep "png" > /home/ronaldo/Escritorio/png.txt
-		png=`egrep -c "png" /home/ronaldo/Escritorio/png.txt`
-
-		find ${TMPDIR} -type f | egrep "odt" > /home/ronaldo/Escritorio/odt.txt
-		odt=`egrep -c "odt" /home/ronaldo/Escritorio/odt.txt`
-
-		#find ${TMPDIR} -type f | egrep "*" > /home/ronaldo/Escritorio/otros.txt
-		#otros=`egrep -c "*" /home/ronaldo/Escritorio/otros.txt`
-
-      suma=$(($txt+$pdf+$mp3+$png+$odt))
-      otros=$(($var1-$suma))
-
-		echo "La cantidad de ficheros es: "${var1}
-		echo "lo que guarda txt es: "${txt}
-      echo "lo que guarda pdf es: "${pdf}
-      echo "lo que guarda mp3 es: "${mp3}
-      echo "lo que guarda png es: "${png}
-      echo "lo que guarda odt es: "${odt}
-      echo "lo que guarda otros es: "${otros}
-		echo "Lo que guarda varl1 es: "$varl1
-		
-#- archivos regulares
-#d directorios
-#c dispositivos de caracter
-#b dispositivos de bloque
-#s sockets
-#p tuberías
-#l links		
-=======
-if [ "$0"  == ./Getopts.sh -a ! -d "$1" ]; then
-	logger -p error -t GESTOR_ALARMAS Falta Argumento válido
-	echo "Missing dirname ("./Getopts.sh --help" for help)"
-
-
-elif [ "$0"  == ./Getopts.sh -a -d "$1" ] ; then
-	TMPDIR=$1
-   echo ""
-	echo "El directorio que usted ingreso es: "$TMPDIR
-
-	ArchivosReglaresT=`find ${TMPDIR} -type f | egrep -c "*"`
-      echo "La cantidad total de archivos regulares es: "${ArchivosReglaresT}
-      echo ""
-
+   ArchivosReglaresT=`find ${TMPDIR} -maxdepth 1 -type f | egrep -c "*" 2>/dev/null`
+      
       #IMAGENES:
-      png=`find ${TMPDIR} -type f | grep -E -c "*\.png$"`
-      cdr=`find ${TMPDIR} -type f | grep -E -c "*\.cdr$"`
-      cpt=`find ${TMPDIR} -type f | grep -E -c "*\.cpt$"`
-      jpeg=`find ${TMPDIR} -type f | grep -E -c "*\.jpeg$"`
-      jfif=`find ${TMPDIR} -type f | grep -E -c "*\.jfif$"`
-      ppt=`find ${TMPDIR} -type f | grep -E -c "*\.ppt$"`
-      pps=`find ${TMPDIR} -type f | grep -E -c "*\.pps$"`
-      jpg=`find ${TMPDIR} -type f | grep -E -c "*\jpg$"`
-      raw=`find ${TMPDIR} -type f | grep -E -c "*\raw$"`
-      psd=`find ${TMPDIR} -type f | grep -E -c "*\psd$"`
-      tiff=`find ${TMPDIR} -type f | grep -E -c "*\tiff$"`
-      xcf=`find ${TMPDIR} -type f | grep -E -c "*\xcf$"`
-      gif=`find ${TMPDIR} -type f | grep -E -c "*\gif$"`
-      eps=`find ${TMPDIR} -type f | grep -E -c "*\eps$"`
-      dng=`find ${TMPDIR} -type f | grep -E -c "*\dng$"`
-      psb=`find ${TMPDIR} -type f | grep -E -c "*\psb$"`
-      jp2=`find ${TMPDIR} -type f | grep -E -c "*\jp2$"`
-      		sumaImagenes=$(($png+$cdr+$cpt+$jpeg+$jfif+$ppt+$pps+$jpg+$raw+$psd+$tiff+$xcf+$gif+$eps+$dng+$psb+$jp2))
-            echo "IMAGENES: "$sumaImagenes
+      sumaImagenes=`find ${TMPDIR} -maxdepth 1 -type f | grep -E -c "*\.(png|cdr|cpt|jpeg|jfif|ppt|pps|jpg|raw|psd|tiff|xcf|gif|eps|dng|psb|jp2|JPG)$" 2>/dev/null`
 
       #DOCUMENTOS:
-      txt=`find ${TMPDIR} -type f | grep -E -c "*\.txt$"`
-      css=`find ${TMPDIR} -type f | grep -E -c "*\.css$"`
-      htm=`find ${TMPDIR} -type f | grep -E -c "*\.htm$"`
-      ps=`find ${TMPDIR} -type f | grep -E -c "*\.ps$"`
-      xlsx=`find ${TMPDIR} -type f | grep -E -c "*\.xlsx$"`
-      ppt=`find ${TMPDIR} -type f | grep -E -c "*\.txt$"`
-      odf=`find ${TMPDIR} -type f | grep -E -c "*\.odf$"`
-      odt=`find ${TMPDIR} -type f | grep -E -c "*\.odt$"`
-      ods=`find ${TMPDIR} -type f | grep -E -c "*\.ods$"`
-      odg=`find ${TMPDIR} -type f | grep -E -c "*\.odg$"`
-      odp=`find ${TMPDIR} -type f | grep -E -c "*\.odp$"`
-      pdf=`find ${TMPDIR} -type f | grep -E -c "*\.pdf$"`
-      ott=`find ${TMPDIR} -type f | grep -E -c "*\.ott$"`
-      fodt=`find ${TMPDIR} -type f | grep -E -c "*\.fodt$"`
-      uot=`find ${TMPDIR} -type f | grep -E -c "*\.uot$"`
-      docx=`find ${TMPDIR} -type f | grep -E -c "*\.docx$"`
-      xml=`find ${TMPDIR} -type f | grep -E -c "*\.xml$"`
-      doc=`find ${TMPDIR} -type f | grep -E -c "*\.doc$"`
-      dot=`find ${TMPDIR} -type f | grep -E -c "*\.dot$"`
-      html=`find ${TMPDIR} -type f | grep -E -c "*\.html$"`
-      rtf=`find ${TMPDIR} -type f | grep -E -c "*\.rtf$"`
-         sumaDocumentos=$(($txt+$odf+$odt+$ods+$odg+$odp+$pdf+$ott+$fodt+$uot+$docx+$xml+$doc+$dot+$html+$rtf))
-         echo "DOCUMENTOS: "$sumaDocumentos
+      sumaDocumentos=`find ${TMPDIR} -maxdepth 1 -type f | grep -E -c "*\.(txt|css|htm|ps|xlsx|txt|odf|odt|ods|odg|odp|pdf|ott|fodt|uot|docx|xml|doc|dot|html|rtf)$" 2>/dev/null`
 
       #AUDIO:
-      cda=`find ${TMPDIR} -type f | grep -E -c "*\.cda$"`
-      mp3=`find ${TMPDIR} -type f | grep -E -c "*\.mp3$"`
-      ogg=`find ${TMPDIR} -type f | grep -E -c "*\.ogg$"`
-      wav=`find ${TMPDIR} -type f | grep -E -c "*\.wav$"`
-      au=`find ${TMPDIR} -type f | grep -E -c "*\.au$"`
-      uLaw=`find ${TMPDIR} -type f | grep -E -c "*\.uLaw$"`
-      MuLaw=`find ${TMPDIR} -type f | grep -E -c "*\.MuLaw$"`
-      aiff=`find ${TMPDIR} -type f | grep -E -c "*\.aiff$"`
-      mid=`find ${TMPDIR} -type f | grep -E -c "*\.mid$"`
-      midi=`find ${TMPDIR} -type f | grep -E -c "*\.midi$"`
-      imi=`find ${TMPDIR} -type f | grep -E -c "*\.rmi$"`
-      wav=`find ${TMPDIR} -type f | grep -E -c "*\.wav$"`
-      ra=`find ${TMPDIR} -type f | grep -E -c "*\.ra$"`
-         sumaAudio=$(($cda+$mp3+$ogg+$wav+$au+$uLaw+$MuLaw+$aiff+$mid+$midi+$rmi+$wav+$ra))
-         echo "AUDIO: "$sumaAudio
+      sumaAudio=`find ${TMPDIR} -maxdepth 1 -type f | grep -E -c "*\.(cda|mp3|ogg|wav|au|uLaw|MuLaw|aiff|mid|midi|rmi|wav|ra)$" 2>/dev/null`
          
       #VIDEO:
-      avi=`find ${TMPDIR} -type f | grep -E -c "*\.avi$"`
-      mov=`find ${TMPDIR} -type f | grep -E -c "*\.mov$"`
-      movie=`find ${TMPDIR} -type f | grep -E -c "*\.movie$"`
-      mpg=`find ${TMPDIR} -type f | grep -E -c "*\.mpg$"`
-      mpeg=`find ${TMPDIR} -type f | grep -E -c "*\.mpeg$"`
-      qt=`find ${TMPDIR} -type f | grep -E -c "*\.qt$"`
-      ram=`find ${TMPDIR} -type f | grep -E -c "*\.ram$"`
-      mp4=`find ${TMPDIR} -type f | grep -E -c "*\.mp4$"`
-      wmv=`find ${TMPDIR} -type f | grep -E -c "*\.wmv$"`
-      ogv=`find ${TMPDIR} -type f | grep -E -c "*\.ogv$"`
-         sumaVideo=$(($avi+$mov+$movie+$mpg+$mpeg+$qt+$ram+$mp4+$wmv+$ogv))
-         echo "VIDEO: "$sumaVideo
+      sumaVideo=`find ${TMPDIR} -maxdepth 1 -type f | grep -E -c "*\.(avi|mov|movie|mpg|mpeg|qt|ram|mp4|wmv|ogv|AVI)$" 2>/dev/null`
 
       #COMPRIMIDOS:
-      gz=`find ${TMPDIR} -type f | grep -E -c "*\.gz$"`
-      gzip=`find ${TMPDIR} -type f | grep -E -c "*\.gzip$"`
-      tar=`find ${TMPDIR} -type f | grep -E -c "*\.tar$"`
-      targz=`find ${TMPDIR} -type f | grep -E -c "*\.tar.gz$"`
-      tarZ=`find ${TMPDIR} -type f | grep -E -c "*\.tar.Z$"`
-      tgz=`find ${TMPDIR} -type f | grep -E -c "*\.tgz$"`
-      zip=`find ${TMPDIR} -type f | grep -E -c "*\.zip$"`
-      arj=`find ${TMPDIR} -type f | grep -E -c "*\.arj$"`
-      rar=`find ${TMPDIR} -type f | grep -E -c "*\.rar$"`
-      z=`find ${TMPDIR} -type f | grep -E -c "*\.Z$"`     
-         sumaComprimidos=$(())
-         echo "COMPRIMIDOS: "$sumaComprimidos
+      sumaComprimidos=`find ${TMPDIR} -maxdepth 1 -type f | grep -E -c "*\.(gz|gzip|tar|tar.gz|tar.Z|tgz|zip|arj|rar|Z)$" 2>/dev/null`
 
-		#find ${TMPDIR} -type f | egrep "mp3" > /var/tmp/Proyecto/pdf.txt
-		#mp3=`egrep -c "mp3" /var/tmp/Proyecto/pdf.txt`
+      #EJECUTABLES
+      sumaEjecutables=`find ${TMPDIR} -maxdepth 1 -type f | grep -E -c "*\.(bas|bat|bin|cfg|dll|com|drv|exe|vxd|elf|pl|py|sh)$" 2>/dev/null`
 
-#http://www.ccee.edu.uy/ensenian/catcomp/material/ArchivosFormatosExtensiones.pdf
-#http://cefire.edu.gva.es/pluginfile.php/277781/mod_resource/content/1/Unidad4/unidad4_html/formatos_de_archivos_y_extensiones.html
-#http://www.taringa.net/comunidades/ubuntuparataringeros/3894103/Pregunta-Extension-de-las-aplicaciones-en-linux.html
+      #CODIGOS FUENTES Y LIBRERIAS
+      sumaCoFuLibrerias=`find ${TMPDIR} -maxdepth 1 -type f | grep -E -c "*\.(a|c|cpp|diff|h|lo|o|so|jar|rkt)$" 2>/dev/null`
+      
+      #ARCHIVOS DEL SISTEMA:
+      sumaArchSistema=`find ${TMPDIR} -maxdepth 1 -type f | grep -E -c "*\.(conf|ko|lock|log|pid|socket|tmp)$" 2>/dev/null`
 
+      suma=$(($sumaImagenes+$sumaDocumentos+$sumaAudio+$sumaVideo+$sumaComprimidos+$sumaEjecutables+$sumaCoFuLibrerias+$sumaArchSistema))
+      		
+      otros=$(($ArchivosReglaresT-$suma))
+}
 
-      		suma=$(($txt+$pdf+$mp3+$png+$odt))
-      		otros=$(($ArchivosReglaresT-$suma))
+function PROCESO_RECURSIVO(){
+   ArchivosReglaresT_A=`find ${TMPDIR} -type f 2>/dev/null`
+   
+   if [ ! $? == 0 ]; then
+      date > /var/Proyecto/ArchivoProyecto
+      echo "Error." >> /var/Proyecto/ArchivoProyecto
+      echo "Su computadora no cumple con los requisitos mínimos para poder ejecutar este comando." >> /var/Proyecto/ArchivoProyecto
+      #echo "Error."
+      #echo "Su computadora no cumple con los requisitos mínimos para poder ejecutar este comando."
+      logger -p auth.error -t PROYECTO_IASGL No cumple requerimientos mínimos
+      exit 1
+   fi
+   ArchivosReglaresT=`find ${TMPDIR} -type f | egrep -c "*"`   
+   
+      #IMAGENES:
+      sumaImagenes=`find ${TMPDIR} -type f | grep -E -c "*\.(png|cdr|cpt|jpeg|jfif|ppt|pps|jpg|raw|psd|tiff|xcf|gif|eps|dng|psb|jp2|JPG)$" 2>/dev/null`
 
+      #DOCUMENTOS:
+      sumaDocumentos=`find ${TMPDIR} -type f | grep -E -c "*\.(txt|css|htm|ps|xlsx|txt|odf|odt|ods|odg|odp|pdf|ott|fodt|uot|docx|xml|doc|dot|html|rtf)$" 2>/dev/null`
+
+      #AUDIO:
+      sumaAudio=`find ${TMPDIR} -type f | grep -E -c "*\.(cda|mp3|ogg|wav|au|uLaw|MuLaw|aiff|mid|midi|rmi|wav|ra)$" 2>/dev/null`
+         
+      #VIDEO:
+      sumaVideo=`find ${TMPDIR} -type f | grep -E -c "*\.(avi|mov|movie|mpg|mpeg|qt|ram|mp4|wmv|ogv|AVI)$" 2>/dev/null`
+
+      #COMPRIMIDOS:
+      sumaComprimidos=`find ${TMPDIR} -type f | grep -E -c "*\.(gz|gzip|tar|tar.gz|tar.Z|tgz|zip|arj|rar|Z)$" 2>/dev/null`
+
+      #EJECUTABLES
+      sumaEjecutables=`find ${TMPDIR} -type f | grep -E -c "*\.(bas|bat|bin|cfg|dll|com|drv|exe|vxd|elf|pl|py|sh)$" 2>/dev/null`
+
+      #CODIGOS FUENTES Y LIBRERIAS
+      sumaCoFuLibrerias=`find ${TMPDIR} -type f | grep -E -c "*\.(a|c|cpp|diff|h|lo|o|so|jar|rkt)$" 2>/dev/null`
+      
+      #ARCHIVOS DEL SISTEMA:
+      sumaArchSistema=`find ${TMPDIR} -type f | grep -E -c "*\.(conf|ko|lock|log|pid|socket|tmp)$" 2>/dev/null`
+
+      suma=$(($sumaImagenes+$sumaDocumentos+$sumaAudio+$sumaVideo+$sumaComprimidos+$sumaEjecutables+$sumaCoFuLibrerias+$sumaArchSistema))
+      		
+      otros=$(($ArchivosReglaresT-$suma))
+}
+
+function NORMAL(){
+   PROCESO
+   if [ "$ArchivosReglaresT" == 0 ]; then
+      echo "El directorio que usted ingreso es: "$TMPDIR
+      echo "El directorio que usted ingreso se encuentra vacío."
+   else
+      echo "El directorio que usted ingreso es: "$TMPDIR > /var/
+      echo "La cantidad total de archivos regulares es: "${ArchivosReglaresT}
       echo ""
-     	echo "lo que guarda otros es: "${otros}
->>>>>>> origin/master
-	
-echo "Es un directoriooooo" 
+      echo "IMAGENES: "$sumaImagenes
+      echo "DOCUMENTOS: "$sumaDocumentos
+      echo "AUDIO: "$sumaAudio
+      echo "VIDEO: "$sumaVideo
+      echo "COMPRIMIDOS: "$sumaComprimidos
+      echo "EJECUTABLES: "$sumaEjecutables
+      echo "LIBRERIAS: "$sumaCoFuLibrerias
+      echo "ARCHIVOS DEL SISTEMA: "$sumaArchSistema
+      echo "OTROS: "${otros}
+   fi
+}
+
+function PORCENTAJE(){
+   PROCESO
+   if [ "$ArchivosReglaresT" == 0 ]; then
+      echo "El directorio que usted ingreso es: "$TMPDIR
+      echo "El directorio que usted ingreso se encuentra vacío."
+   else
+      echo "El directorio que usted ingreso es: "$TMPDIR
+      echo "La cantidad total de archivos regulares es: "${ArchivosReglaresT}
+      echo ""
+      echo "IMAGENES: "$((100*$sumaImagenes/$ArchivosReglaresT))"%"
+      echo "DOCUMENTOS: "$((100*$sumaDocumentos/$ArchivosReglaresT))"%"
+      echo "AUDIO: "$((100*$sumaAudio/$ArchivosReglaresT))"%"
+      echo "VIDEO: "$((100*$sumaVideo/$ArchivosReglaresT))"%"
+      echo "COMPRIMIDOS: "$((100*$sumaComprimidos/$ArchivosReglaresT))"%"
+      echo "EJECUTABLES: "$((100*$sumaEjecutables/$ArchivosReglaresT))"%"
+      echo "LIBRERIAS: "$((100*$sumaCoFuLibrerias/$ArchivosReglaresT))"%"
+      echo "ARCHIVOS DEL SISTEMA: "$((100*$sumaArchSistema/$ArchivosReglaresT))"%"
+      echo "OTROS: "$((100*$otros/$ArchivosReglaresT))"%"
+   fi
+}
+
+function RECURSIVO(){
+   PROCESO_RECURSIVO
+   if [ "$ArchivosReglaresT" == 0 ]; then
+      echo "El directorio que usted ingreso es: "$TMPDIR
+      echo "El directorio que usted ingreso se encuentra vacío."
+   else
+      echo "El directorio que usted ingreso es: "$TMPDIR
+      echo "La cantidad total de archivos regulares es: "${ArchivosReglaresT}
+      echo ""
+      echo "IMAGENES: "$sumaImagenes
+      echo "DOCUMENTOS: "$sumaDocumentos
+      echo "AUDIO: "$sumaAudio
+      echo "VIDEO: "$sumaVideo
+      echo "COMPRIMIDOS: "$sumaComprimidos
+      echo "EJECUTABLES: "$sumaEjecutables
+      echo "LIBRERIAS: "$sumaCoFuLibrerias
+      echo "ARCHIVOS DEL SISTEMA: "$sumaArchSistema
+      echo "OTROS: "${otros}
+   fi
+}
+
+function PORCENTAJE_RECURSIVO(){
+   PROCESO_RECURSIVO
+   if [ "$ArchivosReglaresT" == 0 ]; then
+      echo "El directorio que usted ingreso es: "$TMPDIR
+      echo "El directorio que usted ingreso se encuentra vacío."
+   else
+      echo "El directorio que usted ingreso es: "$TMPDIR
+      echo "La cantidad total de archivos regulares es: "${ArchivosReglaresT}
+      echo ""
+      echo "IMAGENES: "$((100*$sumaImagenes/$ArchivosReglaresT))"%"
+      echo "DOCUMENTOS: "$((100*$sumaDocumentos/$ArchivosReglaresT))"%"
+      echo "AUDIO: "$((100*$sumaAudio/$ArchivosReglaresT))"%"
+      echo "VIDEO: "$((100*$sumaVideo/$ArchivosReglaresT))"%"
+      echo "COMPRIMIDOS: "$((100*$sumaComprimidos/$ArchivosReglaresT))"%"
+      echo "EJECUTABLES: "$((100*$sumaEjecutables/$ArchivosReglaresT))"%"
+      echo "LIBRERIAS: "$((100*$sumaCoFuLibrerias/$ArchivosReglaresT))"%"
+      echo "ARCHIVOS DEL SISTEMA: "$((100*$sumaArchSistema/$ArchivosReglaresT))"%"
+      echo "OTROS: "$((100*$otros/$ArchivosReglaresT))"%"
+   fi
+}
+
+################## FIN DE LAS FUNCIONES ##########################
+
+   while getopts :dhp: arg
+      do
+      case $arg in
+         d)   DPROFUNDIDAD=0 ;;
+         h)   if [ $# == 1 ]; then
+                 AYUDA=1
+              else
+                 echo "Opción -h no se puede combinar."
+                 logger -p error -t PROYECTO_IASGL Combinación inválida
+                 exit 1
+              fi ;;
+         p)   NUMERO=$(echo $OPTARG | egrep --only-matching '^[0-9]+')
+              if [ "$NUMERO" == 1 ]; then
+                 PORCENTAJE=$NUMERO
+              else
+                 echo "Sólo se admite -p1 (Seleccione -h para ver la ayuda)."
+                 logger -p error -t PROYECTO_IASGL Argumento inválido
+                 exit 1
+              fi
+
+              OPCION=$(echo $OPTARG | egrep --only-matching 'd$')
+              if [ "$OPCION" == "d" ]; then
+                 DPROFUNDIDAD=0
+              fi ;;
+         :)   echo "Debe meter un argumento a la opción: -p1"
+              logger -p auth.error -t PROYECTO_IASGL Falta argumento opción -p
+              exit 1 ;;
+         \?)  echo "Opción inválida -$OPTARG ignorada."
+              logger -p auth.error -t PROYECTO_IASGL Opción inválida
+              exit 1 ;;
+      esac
+   done
+
+if [ $# == 0 ]; then
+   echo "Es necesario un directorio (Seleccione -h para ver la ayuda)."
+   logger -p error -t PROYECTO_IASGL Falta directorio
+   exit 1
+elif [ $# == 1 ]; then
+   TMPDIR=$1
+
+   if [ "$AYUDA" == 1 ]; then
+      AYUDA
+      exit 0
+   elif [ -d "$TMPDIR" ]; then
+      if [ "$PORCENTAJE" == 1 ]; then
+         if [ "$DPROFUNDIDAD" == 1 ]; then
+            PORCENTAJE_RECURSIVO
+         else
+            PORCENTAJE
+         fi         
+      else
+         if [ "$DPROFUNDIDAD" == 1 ]; then
+            RECURSIVO
+         else
+            NORMAL
+         fi
+      fi
+
+      exit 0     
+   else
+      echo "Argumento NO es directorio."
+      logger -p error -t PROYECTO_IASGL Falta Argumento válido
+      exit 1
+   fi      
+elif [ $# == 2 ]; then
+   TMPDIR=$2
+
+elif [ $# == 3 ]; then
+   TMPDIR=$3
+
+else
+   echo "Cantidad argumentos inválida."
+   logger -p error -t PROYECTO_IASGL Cantidad argumentos inválida
+   exit 1
 fi
+
+#Verificación si meten parámetros#
+if [ -d "$TMPDIR" ]; then
+   if [ "$PORCENTAJE" == 1 -a "$DPROFUNDIDAD" == 0 ]; then
+      PORCENTAJE
+   elif [ "$PORCENTAJE" == 1 ]; then
+      PORCENTAJE_RECURSIVO
+   elif [ "$DPROFUNDIDAD" == 0 ]; then
+      NORMAL
+   else
+      echo "*Algo salió mal*"
+      logger -p error -t PROYECTO_IASGL Error desconocido
+      exit 1
+   fi   
+else
+   echo "Argumento NO es directorio."
+   logger -p error -t PROYECTO_IASGL Falta Argumento válido
+   exit 1
+fi
+
+exit 0
+
+echo "Cantidad: "$#
+echo "Ayuda: "$AYUDA
+echo "Porcentaje: "$PORCENTAJE
+echo "Profundidad: "$DPROFUNDIDAD
+echo "TMPDIR: "$TMPDIR
 
 exit 0
 
